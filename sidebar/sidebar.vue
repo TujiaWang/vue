e96705eb8a21710dd699dcb4838a680fc154cbdb
tujiawang <wangguangxi88@gmail.com>
2020-06-04 21:54:26 +0800

    新增侧边栏

---
+++ sidebar/sidebar.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-menu class="el-menu-vertical-demo">
+    <subitem :item="menus" />
+  </el-menu>
+</template>
+<script>
+import subitem from "./subitem";
+export default {
+  components: {
+    subitem,
+  },
+  props: {
+    menus: Array,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="less"></style>
---
+++ sidebar/subitem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="navMenu">
+    <div v-for="(navMenu, idx) in item" :key="idx">
+      <el-submenu
+        v-if="navMenu.children && navMenu.children.length > 0"
+        :index="navMenu.path"
+      >
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span> {{ navMenu.label }}</span>
+        </template>
+        <subitem :item="navMenu.children" />
+      </el-submenu>
+      <el-menu-item :index="navMenu.path" v-else>
+        <i class="el-icon-location"></i>
+        <span slot="title">{{ navMenu.label }}</span>
+      </el-menu-item>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    item: Array,
+  },
+  name: "subitem",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="less"></style>
